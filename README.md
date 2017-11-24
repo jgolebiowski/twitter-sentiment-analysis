97a029524041100c90828774cdaadd43e2e6672a
Jacek Golebiowski <jacek.golebiowski14@imperial.ac.uk>
2017-11-24 17:03:08 +0000

    some pytorch tutorials

--- README.md
+++ README.md
@@ -11,9 +11,11 @@ http://help.sentiment140.com/for-students
 http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
 
 Tutorials:
-https://www.cs.toronto.edu/~frank/csc401/
 
+https://www.cs.toronto.edu/~frank/csc401/
 esp:
 https://www.cs.toronto.edu/~frank/csc401/tutorials/csc401_A1_tut2.pdf
 https://www.cs.toronto.edu/~frank/csc401/tutorials/csc401_A1_tut3.pdf
 https://www.cs.toronto.edu/~frank/csc401/tutorials/csc401_A1_tut4.pdf
+
+https://www.oreilly.com/learning/perform-sentiment-analysis-with-lstms-using-tensorflow
---
+++ pytorch/tutorial-1.2.py
@@ -0,0 +1,27 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+
+# Define a NN module
+
+
+class MyNetwork(nn.Module):
+    """My neural network"""
+
+    def __init__(self):
+        super().__init__()
+
+        self.dense1 = nn.Linear(10, 10)
+        self.dense2 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        """Pass forward through the model"""
+        x = self.dense1(x)
+        x = nn.functional.relu(x)
+
+        x = self.dense2(x)
+        x = nn.functional.softmax(x)
+
+
+net = MyNetwork()
+print(net)
